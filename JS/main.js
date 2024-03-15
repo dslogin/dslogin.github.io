076e20f964a368a5ed43add69d703d3c27f23bdb
Danila Sergeevich <dslogin.sh@gmail.com>
2024-03-16 02:41:02 +0300

    v2.0 Полная переработка структуры сайта.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     $('.menu_dropdown').click(function(event) {
-        $('.menu_dropdown,.menu').toggleClass('active');
+        $('.menu_dropdown, .menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
 })
+
+$(document).ready(function() {
+    $('.menu_link').click(function(event) {
+        $('.menu_dropdown, .menu').removeClass('active');
+        $('body').removeClass('lock')
+    });
+});
