eed87fc31410d4d0a6c3302aac0cb84fa573b219
dslogin <dslogin.sh@gmail.com>
2024-04-04 01:05:34 +0300

    v2.1 Исправлены ошибки, доработана структура сайта.

--- JS/main.js
+++ JS/main.js
@@ -3,11 +3,9 @@ $(document).ready(function() {
         $('.menu_dropdown, .menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
-})
 
-$(document).ready(function() {
     $('.menu_link').click(function(event) {
         $('.menu_dropdown, .menu').removeClass('active');
         $('body').removeClass('lock')
     });
-});
+})
